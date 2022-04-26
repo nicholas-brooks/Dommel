e4103e0064d6f245ae2c1d5ac0e0df5d4bd9220f
Nicholas Brooks <nick@nbrooks.id.au>
2022-04-26 11:19:35 +1000

    Add missing XML comments

--- src/Dommel/ComputedColumnAttribute.cs
+++ src/Dommel/ComputedColumnAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace Dommel;
 
+/// <summary>
+/// Define the property as a Computed Column that is calculated from a SQL Expression.
+/// </summary>
 [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
 public class ComputedColumnAttribute : Attribute
 {
@@ -16,14 +19,25 @@ public class ComputedColumnAttribute : Attribute
         alias = null;
     }
 
+    /// <summary>
+    /// Constructs the ComputedColumnAttribute with an Alias.
+    /// </summary>
+    /// <param name="expression">The sql expression for the computed column.</param>
+    /// <param name="alias">The alias to use for this column</param>
     public ComputedColumnAttribute(string expression, string alias)
     {
         Expression = expression;
         this.alias = alias;
     }
     
+    /// <summary>
+    /// SQL Expression to use for this Computed Column
+    /// </summary>
     public string Expression { get; }
 
+    /// <summary>
+    /// Optional Alias name to use for this Computed Column
+    /// </summary>
     public string? Alias
     {
         get => alias;
--- src/Dommel/TableName.cs
+++ src/Dommel/TableName.cs
@@ -1,15 +1,33 @@
 ﻿namespace Dommel;
 
+/// <summary>
+/// Class representing the Table Name tuple (name and optional alias).
+/// </summary>
 public class TableName
 {
+    /// <summary>
+    /// Constructor for TableName
+    /// </summary>
+    /// <param name="name">Name of the Table</param>
+    /// <param name="alias">Alias for the table.  If null or empty will default to Name</param>
     public TableName(string name, string alias)
     {
         Name = name;
         Alias = string.IsNullOrEmpty(alias) ? name : alias;
     }
+    /// <summary>
+    /// Name of the Table.
+    /// </summary>
     public string Name { get; }
+    /// <summary>
+    /// Alias of the Table.
+    /// </summary>
     public string Alias { get; }
 
+    /// <summary>
+    /// String representation of TableName
+    /// </summary>
+    /// <returns>TableName representation</returns>
     public override string ToString()
     {
         return $"{Name} {Alias}";
